cf300703d66bf3ae22533af5030a453f07e83d89
David Li <davidli012345@gmail.com>
2022-08-07 00:11:59 +0000

    fix: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,9 @@ import { astroImageTools } from 'astro-imagetools';
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://friendlyuser.github.io/astro-tech-blog', // Use to generate your sitemap and canonical URLs in your final build.
+  site: 'https://friendlyuser.github.io', // Use to generate your sitemap and canonical URLs in your final build.
   // Important!
+  base: '/astro-tech-blog',
   // Only official '@astrojs/*' integrations are currently supported by Astro.
   // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
   // with 'astro build' command.
