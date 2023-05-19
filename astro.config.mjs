125876e99987410b6c3298e6c103dbd0642e111b
David Li <davidli012345@gmail.com>
2023-05-18 23:00:07 -0700

    fix: nuke expierment flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,9 +15,7 @@ export default defineConfig({
   // Only official '@astrojs/*' integrations are currently supported by Astro.
   // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
   // with 'astro build' command.
-  experimental: {
-    integrations: true,
-  },
+
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
